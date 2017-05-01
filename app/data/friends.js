befc17cfeee18db4f331ec6a9ded82ae95096673
Jacob Wallace <jacob.wallace@utexas.edu>
2017-05-01 17:15:05 -0500

    app successfully compares your answers to other friends in database

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,13 @@
 var friendList = [
 {
 	name: "Joe",
-	photo: "img/img",
-	answerArray: ["5","3","2","1","5","1","2","5","3","2"]
+	photo: "https://pbs.twimg.com/profile_images/552307347851210752/vrXDcTFC.jpeg",
+	answerArray: ["1","1","1","1","1","1","1","1","1","1"]
 },
 {
 	name: "Bob",
-	photo: "img/img",
-	answerArray: ["2","5","1","3","2","1","5","4","4","3"]
+	photo: "http://www.bobthebuilder.com/en-us/Images/btb_where_to_watch_background_Bob_tcm1239-232957.png",
+	answerArray: ["5","5","5","5","5","5","5","5","5","5"]
 }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,4 +6,35 @@ module.exports = function(app) {
 	app.get("/api/friends", function(req, res) {
 		res.json(friendList);
 	});
+
+	app.post("/api/friends", function(req, res) {
+		var friendFound = {
+			name: "",
+			photo: "",
+			score: 1000
+		}
+
+		var userData = req.body;
+
+		var difference = 0;
+
+		for (var i = 0; i < friendList.length; i++) {
+
+			difference = 0;
+
+			for (var x = 0; x < friendList[i].answerArray[x]; x++) {
+				difference += Math.abs(parseInt(userData.answerArray[x]) - parseInt(friendList[i].answerArray[x]));
+				console.log(userData.answerArray[x]); 
+
+				if (difference <= friendFound.score) {
+					friendFound.name = friendList[i].name;
+					friendFound.photo = friendList[i].photo;
+					friendFound.score = finalScore;
+				}
+			}
+		}
+
+		friendList.push(userData);
+		res.json(friendFound);
+	});
 };
