b71619b41059ce131556e3a18a81ab13ef0e1c33
Jacob Wallace <jacob.wallace@utexas.edu>
2017-05-01 17:40:17 -0500

    server tweaks

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
+
+var htmlRoutes = require("./app/routing/htmlRoutes.js");
+var apiRoutes = require("./app/routing/apiRoutes.js");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -9,8 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+apiRoutes(app);
+htmlRoutes(app);
 
 app.listen(PORT, function() {
   console.log("Server listening on PORT: " + PORT);
