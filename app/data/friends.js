73544aa6f752e1d8e64174430629ae3ffae4e1de
Jacob Wallace <jacob.wallace@utexas.edu>
2017-05-01 14:34:47 -0500

    displays JSON objects at api/friends correctly, set up server and some routing

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+var friendList = [
+{
+	name: "Joe",
+	photo: "img/img",
+	answerArray: ["5","3","2","1","5","1","2","5","3","2"]
+},
+{
+	name: "Bob",
+	photo: "img/img",
+	answerArray: ["2","5","1","3","2","1","5","4","4","3"]
+}
+];
+
+module.exports = friendList;
